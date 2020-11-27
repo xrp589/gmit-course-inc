fc91de106bae7718d06567484e03a4a9198b739c
xrp589 <G00398124@gmit.ie>
2020-11-27 12:52:29 +0000

    init lab coimmit

--- lab-sensors/Task_1/Task_1.ino
+++ lab-sensors/Task_1/Task_1.ino
@@ -1,38 +1,29 @@
-/*
-  Blink
 
-  Turns an LED on for one second, then off for one second, repeatedly.
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 6;     // the number of the pushbutton pin
+const int ledPin =  2;      // the number of the LED pin
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
- /*
-  Blink
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
 
-  First Task setup up Arduino to blink on breadboard using pin 13
-  Ronan O Boyle 
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
- 
- delay(1000);                       // wait for a second
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
 }
